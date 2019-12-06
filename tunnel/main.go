408960a282455276befad77f1d33aa8740fcd0a8
Jason A. Donenfeld <Jason@zx2c4.com>
2019-12-06 14:29:18 +0100

    tunnel: Add rudimentary captive portal detection
    This doesn't check for DNS hijacking, and the actual captive portal
    detection mechanism is rather weak, but it does lay the groundwork for
    making connections that are exempt from the tunnel. Probably Microsoft's
    server should not be used or hardcoded like that, either. This commit is
    meant to be expanded upon in followups.
    
    `CaptivePortalDetection.TestConnectivity()` should be called from the
    broker process.

--- tunnel/main.go
+++ tunnel/main.go
@@ -6,8 +6,14 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
+	"net/http"
 	"path/filepath"
 	"unsafe"
 
@@ -18,12 +24,15 @@ import (
 
 	"golang.zx2c4.com/wireguard/windows/conf"
 	"golang.zx2c4.com/wireguard/windows/tunnel"
+	"golang.zx2c4.com/wireguard/windows/tunnel/winipcfg"
+	"golang.zx2c4.com/wireguard/windows/tunnel/firewall"
 )
 
 //export WireGuardTunnelService
 func WireGuardTunnelService(confFile16 *uint16) bool {
 	confFile := windows.UTF16ToString((*[(1 << 30) - 1]uint16)(unsafe.Pointer(confFile16))[:])
 	tunnel.UseFixedGUIDInsteadOfDeterministic = true
+	firewall.ExemptBuiltinAdministrators = true
 	conf.PresetRootDirectory(filepath.Dir(confFile))
 	err := tunnel.Run(confFile)
 	if err != nil {
@@ -44,6 +53,73 @@ func WireGuardGenerateKeypair(publicKey *byte, privateKey *byte) {
 	privateKeyArray[31] = (privateKeyArray[31] & 127) | 64
 
 	curve25519.ScalarBaseMult(publicKeyArray, privateKeyArray)
+}
+
+func findPhysicalDefaultRoute() (winipcfg.LUID, net.IP, error) {
+	r, err := winipcfg.GetIPForwardTable2(windows.AF_INET)
+	if err != nil {
+		return 0, nil, err
+	}
+	lowestMetric := ^uint32(0)
+	var nextHop net.IP
+	var luid winipcfg.LUID
+	for i := range r {
+		if r[i].DestinationPrefix.PrefixLength != 0 {
+			continue
+		}
+		ifrow, err := r[i].InterfaceLUID.Interface()
+		if err != nil || ifrow.OperStatus != winipcfg.IfOperStatusUp || ifrow.MediaType == winipcfg.NdisMediumIP {
+			continue
+		}
+		if r[i].Metric < lowestMetric {
+			lowestMetric = r[i].Metric
+			nextHop = r[i].NextHop.IP()
+			luid = r[i].InterfaceLUID
+		}
+	}
+	if len(nextHop) == 0 {
+		return 0, nil, errors.New("Unable to find default route")
+	}
+	return luid, nextHop, nil
+}
+
+//export TestConnectivity
+func TestConnectivity() int32 {
+	const (
+		exemptIP = "13.107.4.52"
+		host     = "www.msftconnecttest.com"
+		url      = "http://%s/connecttest.txt"
+	)
+
+	luid, nextHop, err := findPhysicalDefaultRoute()
+	if err != nil {
+		return -1
+	}
+	destination := net.IPNet{IP: net.ParseIP(exemptIP), Mask: net.IPv4Mask(255, 255, 255, 255)}
+	err = luid.AddRoute(destination, nextHop, 0)
+	if err != nil {
+		return -1
+	}
+	defer luid.DeleteRoute(destination, nextHop)
+
+	req, err := http.NewRequest("GET", fmt.Sprintf(url, exemptIP), nil)
+	if err != nil {
+		return -1
+	}
+	req.Host = host
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return -1
+	}
+	text, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return -1
+	}
+	if bytes.Equal(text, []byte("Microsoft Connect Test")) {
+		return 1
+	} else {
+		return 0
+	}
 }
 
 func main() {}
