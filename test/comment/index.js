cb77d110c90389eea6417c48ec6a6bc11c318aaa
Miao Huang <mhuang@connected.io>
2019-12-17 11:09:27 -0500

    test: only make comment if it is PR

--- test/comment/index.js
+++ test/comment/index.js
@@ -6,7 +6,7 @@ fs.readFile(path, (err, data) => {
     if (err) {
         console.log(`Fail to read file: ${path}, err: ${err.message}`)
     }
-    const regex = /(?<errors>\d*) Error\(s\), (?<warnings>\d*) Warning\(s\)/gm;
+    const regex = /(?<errors>\d*) Error\(s\), (?<warnings>\d*) Warning\(s\)/gm
     const { errors, warnings } = regex.exec(data.toString()).groups
     bot.comment(process.env.GH_AUTH_TOKEN, `<h3>Errors: ${errors}, Warnings: ${warnings}</h3>
     Details: <strong>${bot.artifactLink('test/result/Output.log', 'details')}</strong>
