76a4e6d6e521ecc2d4c5196bb5f46cff24fa47b4
Miao Huang <mhuang@connected.io>
2020-01-15 14:14:13 -0500

    test: set up AWS ec2 to do smoke test
    In order to do smoke test, we need to simulate the real production
    environment which is in Windows 10. However the CircleCI is current
    using Windows server 2019 for all windows job and Windows server 2019
    only has IE 11 installed. IE 11 is not supported by FxA anymore and
    cannot show user login web page properly. This is why we start using VM
    to do smoke test.

---
+++ test/smoke/FirefoxPrivateVPNUITest/FirefoxPrivateVPNUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FirefoxPrivateVPNUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FirefoxPrivateVPNUITest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("716e361c-5621-4cd1-915e-05a7ae769cb0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
