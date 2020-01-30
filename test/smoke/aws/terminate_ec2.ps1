f694dd45b92df673a044e64801ffef25b3b7843b
Miao Huang <mhuang@connected.io>
2020-01-30 10:48:00 -0500

    test: fix the bug on UI tests
    There are a few bugs on UI tests. One of them is that the pipeline is
    not able to terminate the Cloudformation stack because Remove-CFNStack
    command needs user to confirm before terminating. The others are tests
    related such as maximized browser hide the UI of the vpn client and
    notification chevron is not auto closed on vm.

--- test/smoke/aws/terminate_ec2.ps1
+++ test/smoke/aws/terminate_ec2.ps1
@@ -1,6 +1,6 @@
 Set-DefaultAWSRegion -Region us-east-1
 $stack = "guardian-stack-$env:CIRCLE_BUILD_NUM"
 # remove the stack
-Remove-CFNStack -StackName $stack
+Remove-CFNStack -StackName $stack -Force
 # wait stack to delete completely
 Wait-CFNStack -StackName $stack -Status DELETE_COMPLETE
