f2130f984dcf13a5bbe7f6dfecc8e249a8760b0c
Miao Huang <mhuang@connected.io>
2020-02-21 14:57:32 -0500

    test: Add UI test on Device screen
    We add one more UI test on device screen. It verifies the title, number
    of devices, current device name, status.

---
+++ test/smoke/FirefoxPrivateVPNUITest/FirefoxPrivateVPNUITest/Screens/DeviceScreen.cs
@@ -0,0 +1,110 @@
+﻿// <copyright file="DeviceScreen.cs" company="Mozilla">
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+namespace FirefoxPrivateVPNUITest.Screens
+{
+    using System;
+    using System.Threading;
+    using OpenQA.Selenium.Appium;
+    using OpenQA.Selenium.Appium.Windows;
+
+    /// <summary>
+    /// This model is for Device screen.
+    /// </summary>
+    internal class DeviceScreen
+    {
+        private AppiumWebElement backButton;
+        private AppiumWebElement title;
+        private AppiumWebElement numberOfDevices;
+        private AppiumWebElement devicePanelTitle;
+        private AppiumWebElement currentDevice;
+        private AppiumWebElement currentDeviceName;
+        private AppiumWebElement currentDeviceStatus;
+        private AppiumWebElement currentDeviceRemoveDeviceButton;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceScreen"/> class.
+        /// </summary>
+        /// <param name="vpnSession">VPN session.</param>
+        public DeviceScreen(WindowsDriver<WindowsElement> vpnSession)
+        {
+            var deviceView = vpnSession.FindElementByClassName("DevicesView");
+            this.backButton = deviceView.FindElementByName("Back");
+            var textBlocks = deviceView.FindElementsByClassName("TextBlock");
+            this.title = textBlocks[0];
+            this.numberOfDevices = textBlocks[1];
+            var devicePanel = deviceView.FindElementByClassName("ScrollViewer");
+            this.devicePanelTitle = devicePanel.FindElementByClassName("TextBlock");
+            var deviceList = devicePanel.FindElementByAccessibilityId("DeviceList");
+            var deviceListItems = deviceList.FindElementsByClassName("ListBoxItem");
+            this.currentDevice = deviceListItems[0];
+            var currentDeviceTextBlocks = this.currentDevice.FindElementsByClassName("TextBlock");
+            this.currentDeviceName = currentDeviceTextBlocks[0];
+            this.currentDeviceStatus = currentDeviceTextBlocks[1];
+            this.currentDeviceRemoveDeviceButton = this.currentDevice.FindElementByAccessibilityId("DeleteButton");
+        }
+
+        /// <summary>
+        /// Get title on Device screen.
+        /// </summary>
+        /// <returns>The title string.</returns>
+        public string GetTitle()
+        {
+            return this.title.Text;
+        }
+
+        /// <summary>
+        /// Get number of device.
+        /// </summary>
+        /// <returns>The number of device.</returns>
+        public string GetNumberOfDevice()
+        {
+            return this.numberOfDevices.Text;
+        }
+
+        /// <summary>
+        /// Get the device panel title.
+        /// </summary>
+        /// <returns>The device panel title.</returns>
+        public string GetDevicePanelTitle()
+        {
+            return this.devicePanelTitle.Text;
+        }
+
+        /// <summary>
+        /// Get current device name.
+        /// </summary>
+        /// <returns>The current device name.</returns>
+        public string GetCurrentDeviceName()
+        {
+            return this.currentDeviceName.Text;
+        }
+
+        /// <summary>
+        /// Get current device status.
+        /// </summary>
+        /// <returns>The current device status.</returns>
+        public string GetCurrentDeviceStatus()
+        {
+            return this.currentDeviceStatus.Text;
+        }
+
+        /// <summary>
+        /// Get the current device remove button.
+        /// </summary>
+        /// <returns>Current device remove button.</returns>
+        public AppiumWebElement GetCurrentDeviceRemoveButton()
+        {
+            return this.currentDeviceRemoveDeviceButton;
+        }
+
+        /// <summary>
+        /// Click the Back button.
+        /// </summary>
+        public void ClickBackButton()
+        {
+            this.backButton.Click();
+        }
+    }
+}
---
+++ test/smoke/FirefoxPrivateVPNUITest/FirefoxPrivateVPNUITest/Tests/DeviceTest.cs
@@ -0,0 +1,79 @@
+﻿// <copyright file="DeviceTest.cs" company="Mozilla">
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+namespace FirefoxPrivateVPNUITest
+{
+    using System;
+    using System.Net;
+    using System.Text.RegularExpressions;
+    using System.Threading;
+    using FirefoxPrivateVPNUITest.Screens;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using OpenQA.Selenium.Appium.Windows;
+    using RestSharp;
+
+    /// <summary>
+    /// This is to test device screen.
+    /// </summary>
+    [TestClass]
+    public class DeviceTest
+    {
+        private FirefoxPrivateVPNSession vpnClient;
+        private BrowserSession browser;
+
+        /// <summary>
+        /// Initialize browser, vpn client sessions.
+        /// </summary>
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.browser = new BrowserSession();
+            this.vpnClient = new FirefoxPrivateVPNSession();
+        }
+
+        /// <summary>
+        /// Dispose vpn client, browser sessions.
+        /// </summary>
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            this.vpnClient.Dispose();
+            this.browser.Dispose();
+        }
+
+        /// <summary>
+        /// The test steps.
+        /// </summary>
+        [TestMethod]
+        public void TestDevice()
+        {
+            // Switch to VPN client session
+            this.vpnClient.Session.SwitchTo();
+            LandingScreen landingScreen = new LandingScreen(this.vpnClient.Session);
+            landingScreen.ClickGetStartedButton();
+
+            // User Sign In via web browser
+            UserCommonOperation.UserSignIn(this.vpnClient, this.browser);
+
+            // Main Screen
+            this.vpnClient.Session.SwitchTo();
+            MainScreen mainScreen = new MainScreen(this.vpnClient.Session);
+            mainScreen.ClickDeviceListButton();
+
+            // Device Screen
+            DeviceScreen deviceScreen = new DeviceScreen(this.vpnClient.Session);
+            Assert.AreEqual("My devices", deviceScreen.GetTitle());
+            Regex rgx = new Regex(@"^[1-5] of 5$");
+            Assert.IsTrue(rgx.IsMatch(deviceScreen.GetNumberOfDevice()));
+            Assert.AreEqual("Devices with Firefox Private Network installed using your account. Connect up to 5 devices.", deviceScreen.GetDevicePanelTitle());
+            Assert.IsTrue(deviceScreen.GetCurrentDeviceName().Contains(Environment.MachineName));
+            Assert.AreEqual("Current device", deviceScreen.GetCurrentDeviceStatus());
+            Assert.IsFalse(deviceScreen.GetCurrentDeviceRemoveButton().Displayed);
+            deviceScreen.ClickBackButton();
+
+            // Sign out
+            UserCommonOperation.UserSignOut(this.vpnClient);
+        }
+    }
+}
