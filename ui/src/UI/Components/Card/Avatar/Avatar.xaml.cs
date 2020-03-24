6e1d25294bca4e0c02012342f4d366980eaa073f
Carlos Mo <cmo@connected.io>
2020-03-24 15:05:12 -0400

    ui: Remove avatar menu
    The avatar menu is no longer used as per the updated designs.

--- ui/src/UI/Components/Card/Avatar/Avatar.xaml.cs
+++ ui/src/UI/Components/Card/Avatar/Avatar.xaml.cs
@@ -43,17 +43,6 @@ namespace FirefoxPrivateNetwork.UI.Components
         }
 
         /// <summary>
-        /// Gets the application version.
-        /// </summary>
-        public string ApplicationVersion
-        {
-            get
-            {
-                return ProductConstants.GetVersion();
-            }
-        }
-
-        /// <summary>
         /// Gets or sets the profile picture image source.
         /// </summary>
         public ImageSource AvatarImage
@@ -113,84 +102,6 @@ namespace FirefoxPrivateNetwork.UI.Components
                     });
                 }
             }
-        }
-
-        private void NavigateSettings(object sender, RoutedEventArgs e)
-        {
-            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
-
-            var parentView = this.Parent;
-            while (!(parentView is UserControl))
-            {
-                parentView = LogicalTreeHelper.GetParent(parentView);
-            }
-
-            mainWindow.NavigateToView(new SettingsView(parentView as UserControl), MainWindow.SlideDirection.Left);
-        }
-
-        private void ButtonViewLogClick(object sender, RoutedEventArgs e)
-        {
-            LogWindow.ShowLog();
-            AvatarMenu.IsOpen = false;
-        }
-
-        private void ButtonFeedbackClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start(ProductConstants.FeedbackFormUrl);
-            AvatarMenu.IsOpen = false;
-        }
-
-        private void ButtonManageAccountClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start(ProductConstants.FxAAccountManagementUrl);
-            AvatarMenu.IsOpen = false;
-        }
-
-        private void ButtonSettingsClick(object sender, RoutedEventArgs e)
-        {
-            AvatarMenu.IsOpen = false;
-            NavigateSettings(sender, e);
-        }
-
-        private void ButtonDebugClick(object sender, RoutedEventArgs e)
-        {
-            AvatarMenu.IsOpen = false;
-
-            using (var saveDialog = new System.Windows.Forms.SaveFileDialog
-            {
-                Filter = "ZIP Archive|*.zip",
-                Title = "Export debug package",
-            })
-            {
-                if (MessageBox.Show("Thank you for debugging the Firefox Private Network VPN client!\n\nThis utility will export a ZIP file to a directory of your choosing." +
-                    " This file will contain the following:\n\n- A list of your running processes\n- A list of your devices and device drivers\n" +
-                    "- Information about your network interfaces\n- Your computer hardware information\n\n" +
-                    "Along with the VPN tunnel log, the currently available list of VPN servers will also be included.\n" +
-                    "Your Firefox account information and any of your VPN credentials will not be exported.\n\n" +
-                    "Do you wish to proceed?",
-                    "Privacy notice",
-                    MessageBoxButton.YesNo,
-                    MessageBoxImage.Question
-                    ) == MessageBoxResult.Yes)
-                {
-                    saveDialog.ShowDialog();
-
-                    if (saveDialog.FileName != string.Empty)
-                    {
-                        ErrorHandling.DebugDump.CreateDump(saveDialog.FileName);
-                    }
-                }
-            }
-        }
-
-        private void ButtonSignOutClick(object sender, RoutedEventArgs e)
-        {
-            Manager.Account.Logout(removeDevice: true);
-
-            AvatarMenu.IsOpen = false;
-
-            MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
-            mainWindow.NavigateToView(new LandingView(), MainWindow.SlideDirection.Right);
         }
     }
 }
