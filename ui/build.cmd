58c15cb7c09b234bfc2f38867dd5b29a1e97866e
Justin Wood <Callek@gmail.com>
2020-06-30 12:24:58 -0400

    Fix ui build too

--- ui/build.cmd
+++ ui/build.cmd
@@ -8,7 +8,7 @@ cd /d %BUILDDIR% || exit /b 1
 :build
        cmd /c nuget.exe restore -SolutionDirectory .\ || goto :error
        "C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\MSBuild\\Current\\Bin\\amd64\\MSBuild.exe" /t:Rebuild /p:Configuration=Release /p:Platform="x64" || goto :error
-	   cd "%BUILDDIR%\\bin" & zip FirefoxPrivateNetworkVPN.zip -r *
+	   cd "%BUILDDIR%\\bin" & zip MozillaVPN.zip -r *
 	   exit /b 0
 
 :error
